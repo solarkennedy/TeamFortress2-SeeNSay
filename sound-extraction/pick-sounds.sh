920f71cdbaf322c7104aeefebf856ff10f90dc80
Kyle Anderson <kyle@xkyle.com>
2013-05-04 22:03:29 -0700

    merged and streamlined sound conversion

--- sound-extraction/pick-sounds.sh
+++ sound-extraction/pick-sounds.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-mkdir gcf-extract/picked/
-
 for class in pyro demoman medic spy engineer soldier scout sniper heavy
 do
-	find gcf-extract/vo/ -iname ${class}_* -size +100k | grep -v cloakedspy  | \
-        grep -v falling | grep -v pain | grep -v mp3 | grep -v cart | xargs -I {} cp {} gcf-extract/picked/
+        mkdir -p sdcard/$class 2>/dev/null
+        # We need wavs that are decently long, but not too long
+	find gcf-extract/vo/ -iname ${class}_* -size +100k -size -280k | \
+        grep -v cloakedspy  | grep -v falling | grep -v pain | grep -v mp3 | grep -v cart | xargs -I {} cp {} sdcard/$class/{}
+	# We can use stock wavs, they are already in a compatible format 
 done
+
+# do preludes
+mkdir sdcard/preludes
+cd preludes
+find  | xargs -P $THREADS -I {} sox -G {} -t wav --endian little -c 1 -r 22050 ../sdcard/preludes/{}
 
