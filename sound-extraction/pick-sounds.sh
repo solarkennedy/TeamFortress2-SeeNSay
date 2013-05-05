cf9ea07618ec86de01c42a46d82f06ff90494fbf
Kyle Anderson <kyle@xkyle.com>
2013-05-04 22:16:57 -0700

    fixed xargs typo

--- sound-extraction/pick-sounds.sh
+++ sound-extraction/pick-sounds.sh
@@ -3,13 +3,15 @@ for class in pyro demoman medic spy engineer soldier scout sniper heavy
 do
         mkdir -p sdcard/$class 2>/dev/null
         # We need wavs that are decently long, but not too long
-	find gcf-extract/vo/ -iname ${class}_* -size +100k -size -280k | \
-        grep -v cloakedspy  | grep -v falling | grep -v pain | grep -v mp3 | grep -v cart | xargs -I {} cp {} sdcard/$class/{}
+        cd gcf-extract/vo/
+	find -iname ${class}_\* -size +100k -size -280k | \
+        grep -v cloakedspy  | grep -v falling | grep -v pain | grep -v mp3 | grep -v cart | xargs --verbose -I {} cp {} ../../sdcard/$class/
 	# We can use stock wavs, they are already in a compatible format 
+        cd ../../
 done
 
 # do preludes
-mkdir sdcard/preludes
+mkdir sdcard/preludes 2>/dev/null
 cd preludes
-find  | xargs -P $THREADS -I {} sox -G {} -t wav --endian little -c 1 -r 22050 ../sdcard/preludes/{}
+find  | xargs --verbose -P 4 -I {} sox -G {} -t wav --endian little -c 1 -r 22050 ../sdcard/preludes/{}
 
